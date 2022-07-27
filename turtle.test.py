5dc4a7d15872c4957568383d4a2b6cea08496636
Mateus Correia <mateuscorreia.dev@gmail.com>
2022-07-26 22:02:36 -0300

    add a new code in the turtle test

--- turtle.test.py
+++ turtle.test.py
@@ -2,13 +2,24 @@ import turtle
 
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
+slowpoke.color('blue')
+pokey = turtle.Turtle()
+pokey.shape('turtle')
+pokey.color('red')
 
 def make_square(the_turtle):
 
-    for i in range (0,4):     
-        slowpoke.forward(100)
-        slowpoke.right(90)
+    for i in range(0,4):     
+        the_turtle.forward(100)
+        the_turtle.right(90)
 
-make_square(slowpoke)
+def make_spiral(the_turtle):
+    for i in range(0,36):
+        make_square(the_turtle)
+        the_turtle.right(10)
+
+make_spiral(slowpoke)
+pokey.right(5)
+make_spiral(pokey)
 
 turtle.mainloop()
